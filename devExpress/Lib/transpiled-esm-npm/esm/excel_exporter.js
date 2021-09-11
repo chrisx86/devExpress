edea0fe093a02f20ab594cc666421b97d9e89ce4
Yaochi <leog908@gmail.com>
2021-09-11 22:00:26 +0800

    devexpress

---
+++ devExpress/Lib/transpiled-esm-npm/esm/excel_exporter.js
@@ -0,0 +1,14 @@
+import { exportDataGrid } from './exporter/exceljs/export_data_grid';
+import { exportPivotGrid } from './exporter/exceljs/export_pivot_grid';
+/**
+* @name excelExporter
+* @section utils
+*/
+
+/**
+ * @name ExcelPivotGridCell
+ * @namespace DevExpress.excelExporter
+ * @inherits dxPivotGridPivotGridCell
+ */
+
+export { exportDataGrid, exportPivotGrid };
